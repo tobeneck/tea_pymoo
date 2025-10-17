Added fitness+entropy impact, and fixed small bugg in fitness impact calculation

callbacks/general/fitness_entropy_impact_pop_callback.py
@@ -2,14 +2,15 @@ import numpy as np
 
 from tea_pymoo.tracing.t_sampling import TracingTypes
 
-from tea_pymoo.callbacks.general.entropy_impact_inds_callback import Entropy_Impact_Inds_Callback, get_entropy_vector
+from tea_pymoo.callbacks.general.fitness_entropy_impact_inds_callback import Fitness_Entropy_Impact_Inds_Callback
+from tea_pymoo.callbacks.general.entropy_impact_inds_callback import get_entropy_vector
 
 
-class Entropy_Impact_Pop_Callback(Entropy_Impact_Inds_Callback):
+class Fitness_Entropy_Impact_Pop_Callback(Fitness_Entropy_Impact_Inds_Callback):
 
-    def __init__(self, initial_popsize, tracing_type=TracingTypes.TRACE_ID, additional_run_info=None, optimal_inds_only=True, filename="entropy_impact_pop") -> None:
+    def __init__(self, initial_popsize, tracing_type=TracingTypes.TRACE_ID, additional_run_info=None, optimal_inds_only=True, filename="fitness_entropy_impact_pop") -> None:
         '''
-        This callback saves the entropy impact of the initial population for each generation, accumulated for the whole population. It essentially wraps the Entropy_Impact_Inds_Callback.
+        This callback saves the fitness impact of the initial population for each generation, accumulated for the whole population. It essentially wraps the Fitness_Impact_Inds_Callback.
 
         Parameters:
         -----------
@@ -38,36 +39,40 @@ class Entropy_Impact_Pop_Callback(Entropy_Impact_Inds_Callback):
         super().handle_additional_run_info()
 
         if algorithm.problem.n_obj != 1:
-            raise NotImplementedError("Entropy impact for each ind individually is currently only implemented for single objective problems.")
+            raise NotImplementedError("Fitness_Entropy impact for each ind individually is currently only implemented for single objective problems.")
 
         generation = algorithm.n_gen
         population = algorithm.pop
         if self.optimal_inds_only:
             population = algorithm.opt
 
-        entropy_impact = np.zeros( self.max_traceID + 1 )
+        fitness_entropy_impact = np.zeros( self.max_traceID + 1 )
 
+        worst_fitness = population.get("F").max()
         entr = get_entropy_vector(population)
 
         for i in range(0, len(population)):
             if self.tracing_type == TracingTypes.NO_TRACING:
                 return
             elif self.tracing_type == TracingTypes.TRACE_ID:
-                raise NotImplementedError("Entropy impact for each ind individually is currently only implemented for trace vector representation.")
+                raise NotImplementedError("Fitness_Entropy impact for each ind individually is currently only implemented for trace vector representation.")
             elif self.tracing_type == TracingTypes.TRACE_LIST:
-                raise NotImplementedError("Entropy impact for each ind individually is currently only implemented for trace vector representation.")
+                raise NotImplementedError("Fitness_Entropy impact for each ind individually is currently only implemented for trace vector representation.")
             elif self.tracing_type == TracingTypes.TRACE_VECTOR:
-                current_ind_entropy_impact = self.print_traceVector_entropy_impact(population[i], entr)
+                current_ind_fitness_impact = self.print_traceVector_fitness_entropy_impact(population[i], entr, worst_fitness)
 
-                entropy_impact += (current_ind_entropy_impact / (len(population)))
+                current_fitness = population[i].get("F")[0]
+                fd = 1 + np.abs(worst_fitness - current_fitness)
+
+                fitness_entropy_impact += (current_ind_fitness_impact / len(population) )
         
         for key in self.data.keys():
             if key == "generation":
                 self.data[key].append(generation)
             elif key == "traceID_m":
-                self.data[key].append(entropy_impact[-1])
+                self.data[key].append(fitness_entropy_impact[-1])
             elif key[:7] == "traceID":
                 trace_index = int( key.split("_")[1] ) - 1 #there is no traceID 0, we shift everything to 1
-                self.data[key].append(entropy_impact[trace_index])
+                self.data[key].append(fitness_entropy_impact[trace_index])
 
 

callbacks/general/fitness_impact_pop_callback.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-from tea_pymoo.callbacks.data_collector import DataCollector
-
 from tea_pymoo.tracing.t_sampling import TracingTypes
 
 from tea_pymoo.callbacks.general.fitness_impact_inds_callback import Fitness_Impact_Inds_Callback
@@ -63,7 +61,7 @@ class Fitness_Impact_Pop_Callback(Fitness_Impact_Inds_Callback):
                 current_fitness = population[i].get("F")[0]
                 fd = 1 + np.abs(worst_fitness - current_fitness)
 
-                fitness_impact += (current_ind_fitness_impact / (len(population) * fd ))
+                fitness_impact += (current_ind_fitness_impact / len(population) )
         
         for key in self.data.keys():
             if key == "generation":