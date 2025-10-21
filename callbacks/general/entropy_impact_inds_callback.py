Fixed some normalization errors in the impact calculation

callbacks/general/entropy_impact_inds_callback.py
@@ -18,14 +18,15 @@ def get_entropy_vector(population):
     for j in range(n_var):
         trace_column = T[:, j]
         unique_cols = np.unique(trace_column, axis=0)
-        #print("unique cols", unique_cols)
 
         for col in unique_cols:
             matches = np.all(trace_column == col, axis=1) #returns how many times col appears in the trace column
             p_col = matches.sum() / n_individuals
             entropy_vector[j] += ( p_col * np.log2(p_col) )
 
-    return -entropy_vector #don't forget to add -1
+    entropy_vector = -entropy_vector
+    entropy_vector += 1 #add 1 to avoid division by 0
+    return entropy_vector #don't forget to add -1
 
 
 
@@ -57,14 +58,16 @@ class Entropy_Impact_Inds_Callback(DataCollector):
 
         super().__init__(data_keys=data_keys, filename=filename, additional_run_info=additional_run_info)
 
-    def print_traceVector_entropy_impact(self, ind, entropy):        
+    def print_traceVector_entropy_impact(self, ind_idx, population, entropy):        
         entropy_impact = np.zeros( self.max_traceID + 1 )
-        T = ind.get("T")
+        trace_vector = population[ind_idx].get("T")
 
         #scale T with the entropy
-        T = T * entropy[:, np.newaxis]
+        trace_vector= trace_vector * entropy[:, np.newaxis]# essentially, we need to scale with the sum of the entropie for each row (I think)
 
-        entropy_impact = ( T.sum(axis=0)) / ( entropy.sum() ) # essentially, we need to scale with the sum of the entropie for each row (I think)
+        entropy_impact = ( trace_vector.sum(axis=0))
+        entropy_impact = entropy_impact / entropy_impact.sum() # normalize this way due to entropy scaling
+
         return entropy_impact
     
     def notify(self, algorithm):
@@ -92,6 +95,7 @@ class Entropy_Impact_Inds_Callback(DataCollector):
                 raise NotImplementedError("Entropy impact for each ind individually is currently only implemented for trace vector representation.")
             elif self.tracing_type == TracingTypes.TRACE_VECTOR:
                 entropy_impact = self.print_traceVector_entropy_impact(population[i], entr)
+                
 
             for key in self.data.keys():
                 if key == "generation":

callbacks/general/fitness_entropy_impact_pop_callback.py
@@ -2,11 +2,12 @@ import numpy as np
 
 from tea_pymoo.tracing.t_sampling import TracingTypes
 
-from tea_pymoo.callbacks.general.fitness_entropy_impact_inds_callback import Fitness_Entropy_Impact_Inds_Callback
+from tea_pymoo.callbacks.data_collector import DataCollector
+
 from tea_pymoo.callbacks.general.entropy_impact_inds_callback import get_entropy_vector
 
 
-class Fitness_Entropy_Impact_Pop_Callback(Fitness_Entropy_Impact_Inds_Callback):
+class Fitness_Entropy_Impact_Pop_Callback(DataCollector):
 
     def __init__(self, initial_popsize, tracing_type=TracingTypes.TRACE_ID, additional_run_info=None, optimal_inds_only=True, filename="fitness_entropy_impact_pop") -> None:
         '''
@@ -26,15 +27,30 @@ class Fitness_Entropy_Impact_Pop_Callback(Fitness_Entropy_Impact_Inds_Callback):
         for i in range(initial_popsize):
             data_keys.append("traceID_"+str(i+1))
         data_keys.append("traceID_m")
-        super().__init__(
-            initial_popsize=initial_popsize, 
-            tracing_type=tracing_type, 
-            additional_run_info=additional_run_info,
-            optimal_inds_only=optimal_inds_only,
-            filename=filename,
-            data_keys=data_keys
-            )
 
+        self.tracing_type = tracing_type
+        self.max_traceID = initial_popsize
+        self.additional_keys = additional_run_info
+        self.optimal_inds_only = optimal_inds_only
+
+        super().__init__(data_keys=data_keys, filename=filename, additional_run_info=additional_run_info)
+
+    def print_traceVector_fitness_entropy_impact(self, ind_idx, population, entropy):        
+        fitness_entropy_impact = np.zeros( self.max_traceID + 1 )
+
+        trace_vector = population[ind_idx].get("T")
+
+        #scale trace_list with the entropy
+        trace_vector = trace_vector * entropy[:, np.newaxis] # essentially, we need to scale with the sum of the entropie for each row (I think)
+
+        #calculate the fitness scaling
+        worst_fitness = population.get("F").max()
+        fitness_distances = np.abs( worst_fitness - population.get("F").flatten() ) + 1
+
+        fitness_entropy_impact = trace_vector.sum(axis=0) * fitness_distances[ind_idx]
+        fitness_entropy_impact = fitness_entropy_impact / fitness_entropy_impact.sum() #need to normalize like this due to entropy scaling
+        return fitness_entropy_impact
+    
     def notify(self, algorithm):
         super().handle_additional_run_info()
 
@@ -48,8 +64,8 @@ class Fitness_Entropy_Impact_Pop_Callback(Fitness_Entropy_Impact_Inds_Callback):
 
         fitness_entropy_impact = np.zeros( self.max_traceID + 1 )
 
-        worst_fitness = population.get("F").max()
         entr = get_entropy_vector(population)
+        fitnes_differences = np.abs( population.get("F").max() - population.get("F").flatten() ) + 1
 
         for i in range(0, len(population)):
             if self.tracing_type == TracingTypes.NO_TRACING:
@@ -59,12 +75,11 @@ class Fitness_Entropy_Impact_Pop_Callback(Fitness_Entropy_Impact_Inds_Callback):
             elif self.tracing_type == TracingTypes.TRACE_LIST:
                 raise NotImplementedError("Fitness_Entropy impact for each ind individually is currently only implemented for trace vector representation.")
             elif self.tracing_type == TracingTypes.TRACE_VECTOR:
-                current_ind_fitness_impact = self.print_traceVector_fitness_entropy_impact(population[i], entr, worst_fitness)
-
-                current_fitness = population[i].get("F")[0]
-                fd = 1 + np.abs(worst_fitness - current_fitness)
-
-                fitness_entropy_impact += (current_ind_fitness_impact / len(population) )
+                current_ind_fitness_entropy_impact = self.print_traceVector_fitness_entropy_impact(i, population, entr)
+                current_ind_fitness_entropy_impact = current_ind_fitness_entropy_impact * fitnes_differences[i]
+                fitness_entropy_impact += (current_ind_fitness_entropy_impact ) #normalization comes later!
+        
+        fitness_entropy_impact = fitness_entropy_impact / fitnes_differences.sum() # normalize overall
         
         for key in self.data.keys():
             if key == "generation":

callbacks/general/fitness_impact_pop_callback.py
@@ -2,10 +2,10 @@ import numpy as np
 
 from tea_pymoo.tracing.t_sampling import TracingTypes
 
-from tea_pymoo.callbacks.general.fitness_impact_inds_callback import Fitness_Impact_Inds_Callback
+from tea_pymoo.callbacks.data_collector import DataCollector
 
 
-class Fitness_Impact_Pop_Callback(Fitness_Impact_Inds_Callback):
+class Fitness_Impact_Pop_Callback(DataCollector):
 
     def __init__(self, initial_popsize, tracing_type=TracingTypes.TRACE_ID, additional_run_info=None, optimal_inds_only=True, filename="fitness_impact_pop") -> None:
         '''
@@ -25,14 +25,26 @@ class Fitness_Impact_Pop_Callback(Fitness_Impact_Inds_Callback):
         for i in range(initial_popsize):
             data_keys.append("traceID_"+str(i+1))
         data_keys.append("traceID_m")
-        super().__init__(
-            initial_popsize=initial_popsize, 
-            tracing_type=tracing_type, 
-            additional_run_info=additional_run_info,
-            optimal_inds_only=optimal_inds_only,
-            filename=filename,
-            data_keys=data_keys
-            )
+
+        self.tracing_type = tracing_type
+        self.max_traceID = initial_popsize
+        self.additional_keys = additional_run_info
+        self.optimal_inds_only = optimal_inds_only
+
+        super().__init__(data_keys=data_keys, filename=filename, additional_run_info=additional_run_info)
+        
+    def print_traceVector_fitness_impact(self, ind_idx: int, population: np.ndarray) -> np.ndarray:
+        fitness_impact = np.zeros( self.max_traceID + 1 )
+        
+        trace_vector = population[ind_idx].get("T") #shape: (n_var, n_traceIDs)
+
+        worst_fitness = population.get("F").max()
+        fitness_distances = np.abs( worst_fitness - population.get("F").flatten() ) + 1
+
+
+        fitness_impact = trace_vector.sum(axis=0) * fitness_distances[ind_idx]
+        fitness_impact = fitness_impact / fitness_impact.sum() # normalize
+        return fitness_impact
 
     def notify(self, algorithm):
         super().handle_additional_run_info()
@@ -45,9 +57,9 @@ class Fitness_Impact_Pop_Callback(Fitness_Impact_Inds_Callback):
         if self.optimal_inds_only:
             population = algorithm.opt
 
-        fitness_impact = np.zeros( self.max_traceID + 1 )
+        fitnes_differences = np.abs( population.get("F").max() - population.get("F").flatten() ) + 1
 
-        worst_fitness = population.get("F").max()
+        fitness_impact = np.zeros( self.max_traceID + 1 )
         for i in range(0, len(population)):
             if self.tracing_type == TracingTypes.NO_TRACING:
                 return
@@ -56,13 +68,12 @@ class Fitness_Impact_Pop_Callback(Fitness_Impact_Inds_Callback):
             elif self.tracing_type == TracingTypes.TRACE_LIST:
                 raise NotImplementedError("Fitness impact for each ind individually is currently only implemented for trace vector representation.")
             elif self.tracing_type == TracingTypes.TRACE_VECTOR:
-                current_ind_fitness_impact = self.print_traceVector_fitness_impact(population[i], worst_fitness)
-
-                current_fitness = population[i].get("F")[0]
-                fd = 1 + np.abs(worst_fitness - current_fitness)
-
-                fitness_impact += (current_ind_fitness_impact / len(population) )
+                current_ind_fitness_impact = self.print_traceVector_fitness_impact(i, population)
+                current_ind_fitness_impact = current_ind_fitness_impact * fitnes_differences[i]
+                fitness_impact += (current_ind_fitness_impact ) #normalization comes later!
         
+        fitness_impact = fitness_impact / fitnes_differences.sum() # normalize overall
+
         for key in self.data.keys():
             if key == "generation":
                 self.data[key].append(generation)

tracing/t_mutation.py
@@ -50,6 +50,9 @@ class T_Mutation(Mutation):
 
             if p_x == new_c_x : #return the parents trace list if the value was not altered
                 return p_tl
+            
+            if not self.value_dependent_mutation: #return trace list with only mutationID if mutation is value independent
+                return TraceList([TraceTuple(self.mutation_counter, 1.0)])
 
             influence_factor_mut = abs(p_x - new_c_x) / ( abs(p_x) + abs(p_x - new_c_x) )
             influence_factor_old = 1 - influence_factor_mut
@@ -123,8 +126,7 @@ class T_Mutation(Mutation):
 
             if np.equal(parent_X, child_X).all() : #return the parents trace list if the value was not altered. Also avoid divide by 0 error.
                 offspring_T[indIndex] = parents_T[indIndex]
-            else:
-
+            elif self.value_dependent_mutation: #calculate the influence based on the old factors if the mutation operator is value dependent
                 for geneIndex in range(0, len(parent_X)):#build the child trace vectors based on the parents and the influence factors
 
                     if parent_X[geneIndex] == child_X[geneIndex]: #the not mutated case
@@ -136,6 +138,14 @@ class T_Mutation(Mutation):
                         offspring_T[indIndex, geneIndex, :-1] = parents_T[indIndex, geneIndex, :-1] * influence_factor_old
                         oldScaledMutImpact = parents_T[indIndex, geneIndex,  -1] * influence_factor_old
                         offspring_T[indIndex, geneIndex,  -1] = oldScaledMutImpact + influence_factor_mut
+            else: #simple case, just assign the mutation traceID
+                for geneIndex in range(0, len(parent_X)):#build the child trace vectors based on the parents and the influence factors
+
+                    if parent_X[geneIndex] == child_X[geneIndex]: #the not mutated case
+                        offspring_T[indIndex, geneIndex, :] = parents_T[indIndex, geneIndex, :]
+                    else :
+                        offspring_T[indIndex, geneIndex, :-1] = np.zeros( len(offspring_T[indIndex, geneIndex, :-1]) )#parents_T[indIndex, geneIndex, :-1]
+                        offspring_T[indIndex, geneIndex,  -1] = 1.0 #full influence of the mutation
 
         return np.array(offspring_T)
 
@@ -180,13 +190,13 @@ class T_Mutation(Mutation):
         return children_T
 
 
-    def __init__(self, mutation, tracing_type=TracingTypes.NO_TRACING, accumulate_mutations=True, **kwargs):
+    def __init__(self, mutation, tracing_type=TracingTypes.NO_TRACING, accumulate_mutations=True, value_dependent=True, **kwargs):
         super().__init__(prob=mutation.prob, prob_var=mutation.prob_var, **kwargs)
         self.mutation = mutation
         self.tracing_type = tracing_type
         self.accumulate_mutations = accumulate_mutations
         self.mutation_counter = 0
-
+        self.value_dependent_mutation = value_dependent
 
     def _do(self, problem, X):
         return self.mutation._do(problem, X)